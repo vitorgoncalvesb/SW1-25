a1a9ddc77cefb716977491f12eaf295ab452ac7f
Vitor G. <160020798+vitorgoncalvesb@users.noreply.github.com>
2025-02-27 11:03:23 -0300

    Add files via upload

---
+++ aula_03/exercícios/exercicio01.php
@@ -0,0 +1,10 @@
+<?php
+
+    $nome = $_POST['ValueName'];
+
+    $message = $_POST['ValueMessage']; 
+
+    echo "-Todas as emissoras estão malucas pelo anuncío do presidente $nome <br>";
+    echo "Prezados Cidadãos <br>";
+    echo "Sua mensagem para o mundo é: $message <br>";
+?>
---
+++ aula_03/processa01.php
@@ -0,0 +1,9 @@
+<?php
+
+    $nome = $_GET['ValueName'];
+
+    $idade = $_GET['ValueAge'];
+
+    echo "Nome digitado:  $nome <br>";
+    echo "Idade digitada: $idade ano(s) de vida! <br>";
+?>
